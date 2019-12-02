677522bf7826400ec097714f52e526649c8db52d
Warri <warriisaac@outlook.com>
2019-12-03 00:57:38 +0100

    User are now fetched from DB

--- src/pages/UsersList.jsx
+++ src/pages/UsersList.jsx
@@ -1,12 +1,95 @@
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import api from '../api';
+
+import styled from 'styled-components';
+import 'react-table/react-table.css';
+
+const Wrapper = styled.div`
+    padding: 0 40px 40px;
+`
 
 class UsersLists extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: [],
+            columns: [],
+            isLoading: false,
+        }
+    }
+
+    componentDidMount = async () => {
+        this.setState({ isLoading: true })
+
+        await api.getAllUsers().then(users => {
+            this.setState({
+                users: users.data,
+                isLoading: false,
+            })
+        })
+    }
+
     render() {
+        const { users, isLoading } = this.state;
+        console.log('UsersList is showing: ', users);
+        const columns = [
+            {
+                Header: 'ID',
+                accessor: 'id',
+                filterable: true,
+            },
+            {
+                Header: 'First',
+                accessor: 'first',
+                filterable: true,
+            },
+            {
+                Header: 'Last',
+                accessor: 'last',
+                filterable: true,
+            },
+            {
+                Header: 'Email',
+                accessor: 'email',
+                filterable: true,
+            },
+            {
+                Header: 'Phone',
+                accessor: 'phone',
+                filterable: true,
+            },
+            {
+                Header: 'Location',
+                accessor: 'location',
+                filterable: true,
+            },
+            {
+                Header: 'Hobby',
+                accessor: 'hobby',
+                filterable: true,
+            },
+        ]
+
+        let showTable = true;
+        if (!users.length) {
+            showTable = false
+        }
+
         return (
-            <div>
-               <p>View all available users</p> 
-            </div>
-        );
+            <Wrapper>
+                {showTable && (
+                    <ReactTable
+                      data={users}
+                      columns={columns}
+                      loading={isLoading}
+                      defaultPageSize={10}
+                      showPageSizeOptions={true}
+                      minRows={0}
+                    />
+                )}
+            </Wrapper>
+        )
     }
 }
 
